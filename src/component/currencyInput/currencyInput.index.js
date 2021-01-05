83a7ca03f3aa038b41b9a012445cb77025961f85
Aman Pratap Singh <aman.singh@kuliza.com>
2021-01-05 18:29:02 +0530

    Folder structure setup

---
+++ src/component/currencyInput/currencyInput.index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./currencyInput.styles.scss";
+
+export default function CurrencyInput() {
+  return (
+    <div>
+      <h2>Convert your currency</h2>
+    </div>
+  );
+}
